98751012ca4ba0a56a992da14ee9bc9d2966645a
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-05-30 19:25:08 -0400

    Initial Commit

---
+++ UIPageViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIPageViewController
+//
+//  Created by Gennadii Kulikov on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
