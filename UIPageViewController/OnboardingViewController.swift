09c0d16bd52fb8317ecedc1f32f975453154b645
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-05-30 20:33:59 -0400

    Добавляем DataSource

--- UIPageViewController/OnboardingViewController.swift
+++ UIPageViewController/OnboardingViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
-class OnboardingViewController: UIPageViewController {
+class OnboardingViewController: UIPageViewController, UIPageViewControllerDataSource {
+
     lazy var pages: [UIViewController] = {
         let red = UIViewController()
         red.view.backgroundColor = .red
@@ -17,9 +18,43 @@ class OnboardingViewController: UIPageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //устанавливаем OnboardingViewController собственным DataSource
+        dataSource = self
+        
         if let first = pages.first {
             setViewControllers([first], direction: .forward, animated: true, completion: nil)
         }
     }
     
+// MARK: - UIPageViewControllerDataSource
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        //возвращаем предыдущий (относительно переданного viewController) дочерний контроллер
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        //возвращаем следующий (относительно переданного viewController) дочерний контроллер
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < pages.count else {
+            return nil
+        }
+        
+        return pages[nextIndex]
+    }
 }
+
