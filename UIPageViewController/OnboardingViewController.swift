de8c78f412073e2b11b36b11dbdebd3b026b3520
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-05-30 20:11:22 -0400

    Создаём и устанавливаем дочерние контроллеры

--- UIPageViewController/OnboardingViewController.swift
+++ UIPageViewController/OnboardingViewController.swift
@@ -1,5 +1,25 @@
 import UIKit
 
 class OnboardingViewController: UIPageViewController {
+    lazy var pages: [UIViewController] = {
+        let red = UIViewController()
+        red.view.backgroundColor = .red
+        
+        let green = UIViewController()
+        green.view.backgroundColor = .green
+        
+        let blue = UIViewController()
+        blue.view.backgroundColor = .blue
+        
+        return [red, green, blue]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let first = pages.first {
+            setViewControllers([first], direction: .forward, animated: true, completion: nil)
+        }
+    }
     
 }
